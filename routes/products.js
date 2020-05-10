cb4563bd694522c26354264bb2f02a77419b7e30
nidhichovatiya <61475096+nidhi0329@users.noreply.github.com>
2020-05-10 01:33:35 -0400

    all sorted

--- routes/products.js
+++ routes/products.js
@@ -6,47 +6,69 @@ const commentData = require('../data').comments;
 
 //Add Product
 router.post("/add", async (req, res) => {
-    let product = req.body;
-    await productData.addProduct(product);
-    res.render('addproduct', {info: 'Add product success!'});
+
+    try {
+        let product = req.body;
+        await productData.addProduct(product);
+        res.render('addproduct', { info: 'Add product success!' });
+    } catch (e) {
+        res.status(400).json({ "err": 1, "msg": e.message });
+    }
 });
 
 // find the product by id
 router.get("/detail/:id", async (req, res) => {
-    let id = req.params.id;
-    let product = await productData.getProductById(id);
-    let comments = await commentData.getCommentByProduct(id);
-    res.render('detail', {
-        'product': product,
-        'comments': comments
-    });
+
+     try {
+        let id = req.params.id;
+        let product = await productData.getProductById(id);
+        let comments = await commentData.getCommentByProduct(id);
+        res.render('detail', {
+            'product': product,
+            'comments': comments
+        });
+    } catch (e) {
+        res.status(400).json({ "err": 1, "msg": e.message });
+    }
 });
 
 // filter product
 router.get("/category/:class", async (req, res) => {
-    let category = req.params.class;
-    let products = await productData.getAllProduct();
-    products = products.filter(function (product) {
-        return product.tags === category;
-    });
 
-    res.render('category', {
-        "products": products
-    });
+    try {
+        let category = req.params.class;
+        let products = await productData.getAllProduct();
+        products = products.filter(function (product) {
+            return product.tags === category;
+        });
+
+        res.render('category', {
+            "products": products
+        });
+    } catch (e) {
+        res.status(400).json({ "err": 1, "msg": e.message });
+    }
 });
 
 // search product
 router.post("/search", async (req, res) => {
-    let searchKeyword = req.body.searchKeyword;
-    let products = await productData.searchProducts(searchKeyword);
-    console.log(products);
-    res.render('category', {
-        "products": products
-    }); 
+    
+    try {
+        let searchKeyword = req.body.searchKeyword;
+        let products = await productData.searchProducts(searchKeyword);
+        console.log(products);
+        res.render('category', {
+            "products": products
+        });
+    } catch (e) {
+        res.status(400).json({ "err": 1, "msg": e.message });
+    }
 });
 
 // cart page
 router.get("/cart", async (req, res) => {
+
+    try{
     let products = [];
     for (let i = 0; i < req.session.user.cart.length; i++) {
         let id = req.session.user.cart[i];
@@ -55,81 +77,150 @@ router.get("/cart", async (req, res) => {
     }
 
     // calculate total money
-    let money = 0;
-    products.forEach(function (product, index) {
-        money += Number.parseFloat(product.price);
-    });
-    res.render('cart', {
-        'products': products,
-        'money': money
-    });
+        let money = 0;
+        products.forEach(function (product, index) {
+            money += Number.parseFloat(product.price);
+        });
+        res.render('cart', {
+            'products': products,
+            'money': money,
+            'orderCount': req.session.user.cart.length
+        });
+    } catch (e) {
+    res.status(400).json({ "err": 1, "msg": e.message });
+    }
 });
 
 // delete product from cart
 router.get("/cart/delete/:id", async (req, res) => {
-    let id = req.params.id;
-    let cart = req.session.user.cart;
-    let index = cart.indexOf(id);
-    req.session.user.cart.splice(index, 1);
-    await userData.updateUser(req.session.user);
-    res.redirect('/products/cart');
+    
+    try {
+        let id = req.params.id;
+        let cart = req.session.user.cart;
+        let index = cart.indexOf(id);
+        req.session.user.cart.splice(index, 1);
+        await userData.updateUser(req.session.user);
+        res.redirect('/products/cart');
+    } catch (e) {
+        res.status(400).json({ "err": 1, "msg": e.message });
+    }
 });
 
 // payment
 router.get("/payment/:money", async (req, res) => {
-    let money = req.params.money;
-    res.render('payment', {
-        'money': money
-    })
+    
+    try {
+        let money = req.params.money;
+        res.render('payment', {
+            'money': money
+        });
+    } catch (e) {
+        res.status(400).json({ "err": 1, "msg": e.message });
+    }
 });
 
 // return order page
 router.get("/orderPage", async (req, res) => {
-    let products = [];
-    for (let i = 0; i < req.session.user.orderHistory.length; i++) {
-        let id = req.session.user.orderHistory[i];
-        let product = await productData.getProductById(id);
-        products.push(product);
+    
+    try {
+        let products = [];
+        for (let i = 0; i < req.session.user.orderHistory.length; i++) {
+            let id = req.session.user.orderHistory[i];
+            let product = await productData.getProductById(id);
+            products.push(product);
+        }
+        res.render('order', {
+            'products': products
+        });
+    } catch (e) {
+        res.status(400).json({ "err": 1, "msg": e.message });
     }
-    res.render('order', {
-        'products': products
-    });
 });
 
 // order and then clear cart
 router.get("/order", async (req, res) => {
-    // update order history
-    let order = req.session.user.orderHistory.concat(req.session.user.cart);
-    req.session.user.orderHistory = order;
-    await userData.updateUser(req.session.user);
+    try {
 
-    // descent the stocks
-    for (let i = 0; i < req.session.user.cart.length; i++) {
-        let id = req.session.user.cart[i];
-        let product = await productData.getProductById(id);
-        await productData.updateProductStockRemaining(id, Number.parseInt(product.stocks) - 1);
+        // descent the stocks
+        for (let i = 0; i < req.session.user.cart.length; i++) {
+            let id = req.session.user.cart[i];
+            let product = await productData.getProductById(id);
+            await productData.updateProductStockRemaining(id, Number.parseInt(product.stocks) - 1);
+            await productData.productSellCount(id, Number.parseInt(product.sellCount) + 1);
+        }
+
+        // update order history
+        let order = req.session.user.orderHistory.concat(req.session.user.cart);
+        req.session.user.orderHistory = order;
+        req.session.user.cart = [];
+        await userData.updateUser(req.session.user);
+
+        // find all products in order history
+        let products = [];
+        for (let i = 0; i < req.session.user.orderHistory.length; i++) {
+            let id = req.session.user.orderHistory[i];
+            let product = await productData.getProductById(id);
+            products.push(product);
+        }
+
+        res.render('order', {
+            'products': products
+        });
+    } catch (e) {
+        res.status(400).json({ "err": 1, "msg": e.message });
     }
-    req.session.user.cart = [];
-
-    // find all products in order history
-    let products = [];
-    for (let i = 0; i < req.session.user.orderHistory.length; i++) {
-        let id = req.session.user.orderHistory[i];
-        let product = await productData.getProductById(id);
-        products.push(product);
-    }
-
-    res.render('order', {
-        'products': products
-    });
 });
 
 router.get("/order/delete/:id", async (req, res) => {
-    let id = req.params.id;
-    let order = req.session.user.orderHistory;
-    let index = order.indexOf(id);
-    req.session.user.orderHistory.splice(index, 1);
-    await userData.updateUser(req.session.user);
-    res.redirect('/products/order');
+    try {
+        let id = req.params.id;
+        let order = req.session.user.orderHistory;
+        let index = order.indexOf(id);
+        req.session.user.orderHistory.splice(index, 1);
+        await userData.updateUser(req.session.user);
+        res.redirect('/products/order');
+    } catch (e) {
+        res.status(400).json({ "err": 1, "msg": e.message });
+    }
 });
+
+// filter by high to low / low to high price and color 
+router.post("/productFilter", async (req, res) => {
+    try {
+        // req.body.color  Should be array
+        let product = await productData.productFilter(req.body.tag, req.body.filter, req.body.color)
+        res.render('category', {
+            "products": product
+        });
+    } catch (e) {
+        res.status(400).json({ "err": 1, "msg": e.message });
+    }
+});
+
+router.get("/productGraph", async (req, res) => {
+    try {
+        let product = await productData.productGraph()
+        res.render('productGraph')
+    } catch (e) {
+        res.status(400).json({ "err": 1, "msg": e.message });
+    }
+});
+
+router.get("/Graph", async (req, res) => {
+    try {
+        let product = await productData.productGraph();
+        var product_data = [];
+        product.forEach(element => {
+
+            product_data.push({
+                x: element.name,
+                y: element.sellCount
+            })
+        })
+        res.json({ "product": product_data })
+    } catch (e) {
+        res.json({ "product": [] })
+    }
+});
+
 module.exports = router;
